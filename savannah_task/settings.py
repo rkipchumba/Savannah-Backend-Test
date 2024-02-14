6516e9928c14f9d5e42c1a48d5488b3a65e86a82
Rodgers Kipchumba <kipchumbarodgers@gmail.com>
2024-02-14 18:18:53 +0300

    fc

--- savannah_task/settings.py
+++ savannah_task/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'rodgers',
         'USER': 'rodgers',
-        'PASSWORD': 'localhost',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
